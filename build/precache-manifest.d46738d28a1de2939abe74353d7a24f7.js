358339823a4462427b011503739a17daae1ca036
yn191032 <62829368+yn191032@users.noreply.github.com>
2020-09-13 13:48:31 +0300

    homepage added, build exec

---
+++ build/precache-manifest.d46738d28a1de2939abe74353d7a24f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f68add93d7bedcdeb60cc2cdc9e79051",
+    "url": "/km-multilink/index.html"
+  },
+  {
+    "revision": "c00794975c1932661de0",
+    "url": "/km-multilink/static/js/2.32065093.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/km-multilink/static/js/2.32065093.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91e458632e4ca29f2a34",
+    "url": "/km-multilink/static/js/main.aded44ac.chunk.js"
+  },
+  {
+    "revision": "9698019edc721b6de229",
+    "url": "/km-multilink/static/js/runtime-main.772da624.js"
+  },
+  {
+    "revision": "644992280bc49e4160eab5af8691bd77",
+    "url": "/km-multilink/static/media/avatar.64499228.jpg"
+  },
+  {
+    "revision": "c20655de6a2434b7d7a6a0ebd5697dba",
+    "url": "/km-multilink/static/media/logo.c20655de.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30a3d5bbf09153d1a6616970b08b1ce0.js"
+  "/km-multilink/precache-manifest.d46738d28a1de2939abe74353d7a24f7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/km-multilink/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
